cfb52accd393ed4cda569bd4cb40abc98b35b77a
F1RLY <firlyalamsudrajat@gmail.com>
2025-09-20 23:01:12 +0700

    Matkul
    buat file model\Matkul.java dan view\MataKuliah.java

--- Model/Matkul.java
+++ Model/Matkul.java
@@ -4,4 +4,16 @@ public class Matkul {
     public String kodeMatkul;
     public String namaMatkul;
     public int sks;
+    public String dosenPengampu;
+
+    public Matkul(String kodeMatkul, String namaMatkul, int sks, String dosenPengampu) {
+        this.kodeMatkul = kodeMatkul;
+        this.namaMatkul = namaMatkul;
+        this.sks = sks;
+        this.dosenPengampu = dosenPengampu;
+    }
+
+    public void lihatMatkul() {
+        System.out.println("Kode Matkul: " + kodeMatkul + ", Nama Matkul: " + namaMatkul + ", SKS: " + sks + ", Dosen Pengampu: " + dosenPengampu);
+    }
 }
